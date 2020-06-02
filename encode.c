aabdd0df7107461e5ec14599b9306f5928b7707d
皮三宝 <43060123+psbazx@users.noreply.github.com>
2020-06-02 15:55:23 +0800

    Update encode.c

--- encode.c
+++ encode.c
@@ -10,8 +10,8 @@ int main()
     FILE* b;
     FILE* c;
     int i,j;
-    const char srcaddr[] = "C:\\Users\\pisanbao\\Desktop\\encode_shell.exe";//src路径
-    const char shelladdr[] = "C:\\Users\\pisanbao\\Desktop\\first.exe";//解壳shell路径
+    const char shelladdr[] = "C:\\Users\\pisanbao\\Desktop\\encode_shell.exe";//shell路径
+    const char srcaddr[] = "C:\\Users\\pisanbao\\Desktop\\first.exe";//src路径
     a = fopen(srcaddr, "rb");
     fseek(a, 0, SEEK_END);
     unsigned long long filesize = ftell(a);
@@ -30,29 +30,42 @@ int main()
     fread(shellbuffer, shellsize, 1, b);
     fclose(b);
     //shell buffer get
-    c = fopen("C:\\Users\\pisanbao\\Desktop\\shell.exe", "ab+");//最终成品
+    c = fopen("C:\\Users\\pisanbao\\Desktop\\shell.exe", "ab+");
     IMAGE_DOS_HEADER * shelldosheader = (IMAGE_DOS_HEADER *)shellbuffer;
     IMAGE_NT_HEADERS* shellntheader = (IMAGE_NT_HEADERS*)((BYTE *)shellbuffer + shelldosheader->e_lfanew);
     IMAGE_SECTION_HEADER * shellsection = (IMAGE_SECTION_HEADER *)((BYTE*)shellbuffer + shelldosheader->e_lfanew + 4 + sizeof(IMAGE_FILE_HEADER) + shellntheader->FileHeader.SizeOfOptionalHeader);
-    for (i = 0; i < shellntheader->FileHeader.NumberOfSections; i++)//遍历节表直至最后一个节
+    for (i = 0; i < shellntheader->FileHeader.NumberOfSections; i++)
         shellsection++;
-    WORD temp = shelldosheader->e_lfanew;//这边保存一波ntheader偏移，因为要通过抹去dos stub来新增节
+    WORD temp = shelldosheader->e_lfanew;
     DWORD tempsize = shellntheader->OptionalHeader.SizeOfHeaders;
-    shelldosheader->e_lfanew = sizeof(IMAGE_DOS_HEADER);
-    //shellntheader->FileHeader.NumberOfSections += 1;            //节表加一
+    shelldosheader->e_lfanew = sizeof(IMAGE_DOS_HEADER);    
+    shellntheader->FileHeader.NumberOfSections += 1;        //节数+1  
+    unsigned long long tempimage = shellntheader->OptionalHeader.SizeOfImage;
     shellntheader->OptionalHeader.SizeOfImage += filentheader->OptionalHeader.SizeOfImage;
-    strcpy((char *)shellsection->Name,".psb");
-    shellsection->Misc.VirtualSize = filentheader->OptionalHeader.SizeOfImage;
-    shellsection->VirtualAddress = shellntheader->OptionalHeader.SizeOfImage;
-    shellsection->SizeOfRawData = filesize;
-    shellsection->PointerToRawData = shellsize;
+    strcpy((char *)shellsection->Name,".psb");//特征名
+    unsigned long long count1;
+    if (filesize % 0x200)
+    {
+        count1 = ((filesize / 0x200) + 1) * 0x200;
+    }
+    else
+    {
+        count1 = filesize;
+    }
+    //新增节主要注意的是要把新增的pe当作一段数据来看
+    IMAGE_SECTION_HEADER* tempsection = shellsection;
+    tempsection--;
+    shellsection->Misc.VirtualSize = filesize;
+    shellsection->VirtualAddress = tempimage;//之前保存的image
+    shellsection->SizeOfRawData = count1;//文件对齐，在这边写的不怎么严谨。。。
+    shellsection->PointerToRawData = tempsection->SizeOfRawData+tempsection->PointerToRawData;//这边踩坑了，有些编译器会在最后节后面加东西导致文件不对齐
     shellsection->Characteristics = 0xe0500060;
-    for (i = temp,j=0; i < tempsize; i++,j++)//循环抹去dos stub，因为会波及到sizeofheader所以这边提前保存了临时变量tempsize
+    for (i = temp,j=0; i < tempsize; i++,j++)
     {
         *((BYTE*)shellbuffer + sizeof(IMAGE_DOS_HEADER)+j) = *((BYTE*)shellbuffer + i);
         *((BYTE*)shellbuffer + i) = 0;
     }
-    fwrite((BYTE*)shellbuffer, shellsize, 1, c);
+    fwrite((BYTE*)shellbuffer, tempsection->SizeOfRawData + tempsection->PointerToRawData, 1, c);
     //新增节完成
     //加密pe最后填充
     /*
