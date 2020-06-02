69c252f9da4eead73f7ac35f9cfec46c099ddb0d
皮三宝 <43060123+psbazx@users.noreply.github.com>
2020-06-02 16:53:36 +0800

    Update encode.c
    代码贼丑。。。

--- encode.c
+++ encode.c
@@ -22,6 +22,8 @@ int main()
     // file buffer get
     IMAGE_DOS_HEADER* filedosheader = (IMAGE_DOS_HEADER*)filebuffer;
     IMAGE_NT_HEADERS* filentheader = (IMAGE_NT_HEADERS*)((BYTE*)filebuffer + filedosheader->e_lfanew);
+    IMAGE_SECTION_HEADER* filesection = (IMAGE_SECTION_HEADER*)((BYTE*)filebuffer + filedosheader->e_lfanew + 4 + sizeof(IMAGE_FILE_HEADER) + filentheader->FileHeader.SizeOfOptionalHeader+40*(filentheader->FileHeader.NumberOfSections-1));
+    filesize = filesection->PointerToRawData + filesection->SizeOfRawData;
     b = fopen(shelladdr, "rb");
     fseek(b, 0, SEEK_END);
     unsigned long long shellsize = ftell(b);
@@ -39,9 +41,17 @@ int main()
     WORD temp = shelldosheader->e_lfanew;
     DWORD tempsize = shellntheader->OptionalHeader.SizeOfHeaders;
     shelldosheader->e_lfanew = sizeof(IMAGE_DOS_HEADER);    
-    shellntheader->FileHeader.NumberOfSections += 1;        //节数+1  
-    unsigned long long tempimage = shellntheader->OptionalHeader.SizeOfImage;
-    shellntheader->OptionalHeader.SizeOfImage += filentheader->OptionalHeader.SizeOfImage;
+    unsigned long long realimage = filesize;
+
+    if (filesize % 0x1000)
+    {
+        realimage = ((filesize / 0x1000) + 1) * 0x1000;
+    }
+    else
+    {
+        realimage = filesize;
+    }
+    //unsigned long long tempimage = shellntheader->OptionalHeader.SizeOfImage;
     strcpy((char *)shellsection->Name,".psb");//特征名
     unsigned long long count1;
     if (filesize % 0x200)
@@ -55,17 +65,29 @@ int main()
     //新增节主要注意的是要把新增的pe当作一段数据来看
     IMAGE_SECTION_HEADER* tempsection = shellsection;
     tempsection--;
+    unsigned long long temptemp;
+    if (tempsection->Misc.VirtualSize % 0x1000)
+    {
+        temptemp = ((tempsection->Misc.VirtualSize / 0x1000) + 1) * 0x1000;
+    }
+    else
+    {
+        temptemp = tempsection->Misc.VirtualSize;
+    }
+    unsigned long long realsize = tempsection->SizeOfRawData + tempsection->PointerToRawData;
     shellsection->Misc.VirtualSize = filesize;
-    shellsection->VirtualAddress = tempimage;//之前保存的image
-    shellsection->SizeOfRawData = count1;//文件对齐，在这边写的不怎么严谨。。。
-    shellsection->PointerToRawData = tempsection->SizeOfRawData+tempsection->PointerToRawData;//这边踩坑了，有些编译器会在最后节后面加东西导致文件不对齐
+    shellsection->VirtualAddress = tempsection->VirtualAddress + temptemp;
+    shellsection->SizeOfRawData = count1;
+    shellsection->PointerToRawData = realsize;//这边踩坑了，有些编译器会在最后节后面加东西导致文件不对齐
     shellsection->Characteristics = 0xe0500060;
+    shellntheader->FileHeader.NumberOfSections += 1;        //节数+1  
+    shellntheader->OptionalHeader.SizeOfImage = shellsection->VirtualAddress + realimage;
     for (i = temp,j=0; i < tempsize; i++,j++)
     {
         *((BYTE*)shellbuffer + sizeof(IMAGE_DOS_HEADER)+j) = *((BYTE*)shellbuffer + i);
         *((BYTE*)shellbuffer + i) = 0;
     }
-    fwrite((BYTE*)shellbuffer, tempsection->SizeOfRawData + tempsection->PointerToRawData, 1, c);
+    fwrite((BYTE*)shellbuffer, realsize, 1, c);
     //新增节完成
     //加密pe最后填充
     /*
