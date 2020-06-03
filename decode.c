f3fdc6550c7042a6f64b96a0a2ef167f0cf613c5
皮三宝 <43060123+psbazx@users.noreply.github.com>
2020-06-03 11:15:59 +0800

    Update decode.c

--- decode.c
+++ decode.c
@@ -15,7 +15,7 @@ void DoRelocation(LPVOID pFileBuffer, void* OldBase, void* NewBase);
 BOOL HasRelocationTable(LPVOID pFileBuffer);
 int main()
 {
-    WCHAR shellDirectory[100] = L"C:\\Users\\pisanbao\\Desktop\\shell.exe"; //这是加壳后的程序
+    WCHAR shellDirectory[100] = L"C:\\Users\\pisanbao\\Desktop\\shell.exe"; //encode后程序这边有个坑，win32api通常是宽字符然而自己写的函数不需要
     DWORD encryptSize = 0;
 
     LPVOID encryptFileBuffer = NULL;
@@ -31,22 +31,25 @@ int main()
 	CONTEXT contx;
     contx.ContextFlags = CONTEXT_FULL;
     GetThreadContext(pi.hThread, &contx);
+	//int x = GetLastError();
+	//printf("%d\n", x);
     DWORD dwEntryPoint = contx.Eax;
-    char* baseAddress = (CHAR*)contx.Ebx + 8;
+	DWORD baseAddress;
     TCHAR szBuffer[4] = { 0 };
-    ReadProcessMemory(pi.hProcess, baseAddress, szBuffer, 4, NULL);
+    ReadProcessMemory(pi.hProcess, (LPCVOID)(contx.Ebx + 8), (LPVOID)&baseAddress, 4, NULL);
+	//printf("挂起进程的线程Context.Eax:%p - Context.Ebx + 8:%p\n", contx.Eax, baseAddress);
     int* fileImageBase;
     fileImageBase = (int*)szBuffer;
     DWORD shellImageBase = *fileImageBase;
     UnloadShell(pi.hProcess, shellImageBase);
 	LPVOID p = AllocShellSize("C:\\Users\\pisanbao\\Desktop\\shell.exe", pi.hProcess, encryptFileBuffer);
 	DWORD pEncryptImageSize = 0;
-	LPVOID pEncryptImageBuffer = FileBufferToImageBuffer((BYTE *)encryptFileBuffer, pEncryptImageSize);//将加密的源程序数据拷贝到新分配的内存空间
+	LPVOID pEncryptImageBuffer = FileBufferToImageBuffer((BYTE *)encryptFileBuffer, pEncryptImageSize);
 	unsigned long old;
 	WriteProcessMemory(pi.hProcess, (void*)(contx.Ebx + 8), &p, sizeof(DWORD), &old);
 
 	if (WriteProcessMemory(pi.hProcess, p, pEncryptImageBuffer, pEncryptImageSize, &old))
-	{// 复制PE数据到shell的进程空间中  
+	{
 
 		DWORD encryptFileOEP = 0;
 		DWORD encryptFileImageBase = 0;
@@ -55,19 +58,13 @@ int main()
 
 		contx.ContextFlags = CONTEXT_FULL;
 
-		//修复入口地址为源程序的入口
 		contx.Eax = encryptFileOEP + (DWORD)p;
-		SetThreadContext(pi.hThread, &contx);// 更新主进程的运行环境为源程序的运行环境
+		SetThreadContext(pi.hThread, &contx);
 
 		LPVOID szBufferTemp = malloc(pEncryptImageSize);
 		memset(szBufferTemp, 0, pEncryptImageSize);
 		ReadProcessMemory(pi.hProcess, p, szBufferTemp, pEncryptImageSize, NULL);
-		/*
-		//////这个是测试用的 实际壳程序将加密文件解密后直接跳转到源程序的入口执行：脱壳后程序保存到文件////////
-		MemeryTOFile(szBufferTemp, "111111.exe");
-		///////////////////////////////////////////////////////////////////////////////////////////////////////
-		*/
-		ResumeThread(pi.hThread);// 恢复执行主线程  
+		ResumeThread(pi.hThread);
 		CloseHandle(pi.hThread);
 	}
 	return 0;
@@ -82,7 +79,7 @@ void UnloadShell(HANDLE ProcHnd, unsigned long BaseAddr)
         ZwUnmapViewOfSection = (pfZwUnmapViewOfSection)GetProcAddress(m, "ZwUnmapViewOfSection");
 
         if (ZwUnmapViewOfSection)
-            res = (ZwUnmapViewOfSection((unsigned long)ProcHnd, BaseAddr) == 0);  //取消映射目标进程的内存
+            res = (ZwUnmapViewOfSection((unsigned long)ProcHnd, BaseAddr) == 0);  
         FreeLibrary(m);
     }
     return;
@@ -96,7 +93,7 @@ LPVOID FileBufferToImageBuffer(BYTE * decodebuffer, DWORD& size)
 	PIMAGE_SECTION_HEADER pSectionHeader = NULL;
 	PIMAGE_SECTION_HEADER pSectionHeader_LAST = NULL;
 
-	//Header信息
+
 	pDosHeader = (PIMAGE_DOS_HEADER)decodebuffer;
 	pNTHeader = (PIMAGE_NT_HEADERS)((DWORD)decodebuffer + pDosHeader->e_lfanew);
 	pPEHeader = (PIMAGE_FILE_HEADER)(((DWORD)pNTHeader) + 4);
@@ -115,11 +112,7 @@ LPVOID FileBufferToImageBuffer(BYTE * decodebuffer, DWORD& size)
 		memcpy(pEncryptBuffer + pSectionHeader->VirtualAddress, decodebuffer + pSectionHeader->PointerToRawData, pSectionHeader->SizeOfRawData);
 		pSectionHeader++;
 	}
-	BYTE * pNew = pEncryptBuffer;
 
-	BYTE * pOld = (BYTE*)((DWORD)decodebuffer + pSectionHeader_LAST->PointerToRawData);
-
-	pEncryptBuffer = pOld;
 
 	return pEncryptBuffer;
 }
@@ -146,7 +139,7 @@ LPVOID GetLastSecData(LPSTR lpszFile, DWORD& fileSize)
 	PIMAGE_SECTION_HEADER pSectionHeader = NULL;
 	PIMAGE_SECTION_HEADER pSectionHeader_LAST = NULL;
 
-	//Header信息
+
 	pDosHeader = (PIMAGE_DOS_HEADER)pFileBuffer;
 	pNTHeader = (PIMAGE_NT_HEADERS)((DWORD)pFileBuffer + pDosHeader->e_lfanew);
 	pPEHeader = (PIMAGE_FILE_HEADER)(((DWORD)pNTHeader) + 4);
@@ -184,7 +177,7 @@ LPVOID AllocShellSize(LPSTR shellDirectory, HANDLE shellProcess, LPVOID encryptF
 	DWORD encryptImageBase = 0;
 	DWORD encryptImageSize = 0;
 
-	//获得ImageBase ImageSize， 进行信息比较
+
 	GetNtHeaderInfo(pShellBuffer, shellImageBase, shellImageSize);
 	GetNtHeaderInfo(encryptFileBuffer, encryptImageBase, encryptImageSize);
 
@@ -196,32 +189,22 @@ LPVOID AllocShellSize(LPSTR shellDirectory, HANDLE shellProcess, LPVOID encryptF
 
 	void* p = NULL;
 
-	//在指定进程的指定位置分配内存
-	/*
-		VirtualAllocEx：在指定进程的虚拟空间保留或提交内存区域，除非指定MEM_RESET参数，否则将该内存区域置0。
-		LPVOID VirtualAllocEx(
-		HANDLE hProcess, // 申请内存所在的进程句柄
-		LPVOID lpAddress, // 保留页面的内存地址；一般用NULL自动分配
-		SIZE_T dwSize, // 欲分配的内存大小，字节单位；注意实际分 配的内存大小是页内存大小的整数倍
-		DWORD flAllocationType,
-		DWORD flProtect
-	*/
-	//如果指定位置内存没有被占用，则取 MAX(shellImageSize,encryptImageSize)
+	
 	if (shellImageBase == encryptImageBase)
 	{
 		shellImageSize = (shellImageSize >= encryptImageSize) ? shellImageSize : encryptImageSize;
-		// 最小的分配方式，具体用法查MSDN，分配失败会返回NULL
+		
 		p = VirtualAllocEx(shellProcess, (void*)shellImageBase, shellImageSize, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 		//int x = GetLastError();
 		//printf("%d\n", x);
 	}
 
-	// 指定位置被占用 & 进程中有重定位表
+
 	if ((p == NULL) && HasRelocationTable(encryptFileBuffer)) {
-		//任意位置分配空间
+		
 		p = VirtualAllocEx(shellProcess, NULL, encryptImageSize, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 
-		//重定位处理
+	
 		if (p) {
 			DoRelocation(encryptFileBuffer, (void*)encryptImageBase, p);
 		}
@@ -250,7 +233,7 @@ VOID GetNtHeaderInfo(LPVOID pFileBuffer, DWORD& ImageBase, DWORD& ImageSize)
 		return;
 	}
 
-	//MZ标志
+	
 	if (*((PWORD)pFileBuffer) != IMAGE_DOS_SIGNATURE)
 	{
 		printf("不是有效的MZ标志\n");
@@ -258,10 +241,10 @@ VOID GetNtHeaderInfo(LPVOID pFileBuffer, DWORD& ImageBase, DWORD& ImageSize)
 		return;
 	}
 
-	//DOS头
+	
 	pDosHeader = (PIMAGE_DOS_HEADER)pFileBuffer;
 
-	//判断是否是有效的PE 
+	
 	if (*((PDWORD)((DWORD)pFileBuffer + pDosHeader->e_lfanew)) != IMAGE_NT_SIGNATURE)
 	{
 		printf("不是有效的PE标志\n");
@@ -271,14 +254,12 @@ VOID GetNtHeaderInfo(LPVOID pFileBuffer, DWORD& ImageBase, DWORD& ImageSize)
 
 	pNTHeader = (PIMAGE_NT_HEADERS)((DWORD)pFileBuffer + pDosHeader->e_lfanew);
 
-	//NT头
+	
 	pPEHeader = (PIMAGE_FILE_HEADER)(((DWORD)pNTHeader) + 4);
 
-	//可选择PE头
+
 	pOptionHeader = (PIMAGE_OPTIONAL_HEADER32)((DWORD)pPEHeader + IMAGE_SIZEOF_FILE_HEADER);
 
-
-	//获取信息
 	ImageBase = pOptionHeader->ImageBase;
 	ImageSize = pOptionHeader->SizeOfImage;
 
@@ -299,17 +280,17 @@ VOID GetEncryptFileContext(LPVOID pFileBuffer, DWORD& OEP, DWORD& ImageBase)
 		return;
 	}
 
-	//MZ标志
+	
 	if (*((PWORD)pFileBuffer) != IMAGE_DOS_SIGNATURE)
 	{
 		printf("不是有效的MZ标志\n");
 		free(pFileBuffer);
 		return;
 	}
-	//DOS头
+
 	pDosHeader = (PIMAGE_DOS_HEADER)pFileBuffer;
 
-	//判断是否是有效的PE 
+	
 	if (*((PDWORD)((DWORD)pFileBuffer + pDosHeader->e_lfanew)) != IMAGE_NT_SIGNATURE)
 	{
 		printf("不是有效的PE标志\n");
@@ -319,13 +300,13 @@ VOID GetEncryptFileContext(LPVOID pFileBuffer, DWORD& OEP, DWORD& ImageBase)
 
 	pNTHeader = (PIMAGE_NT_HEADERS)((DWORD)pFileBuffer + pDosHeader->e_lfanew);
 
-	//PE头
+	
 	pPEHeader = (PIMAGE_FILE_HEADER)(((DWORD)pNTHeader) + 4);
 
-	//可选择PE头
+	
 	pOptionHeader = (PIMAGE_OPTIONAL_HEADER32)((DWORD)pPEHeader + IMAGE_SIZEOF_FILE_HEADER);
 
-	//获取信息
+
 	OEP = pOptionHeader->AddressOfEntryPoint;
 	ImageBase = pOptionHeader->ImageBase;
 
@@ -364,14 +345,14 @@ BOOL HasRelocationTable(LPVOID pFileBuffer)
 	PIMAGE_SECTION_HEADER pSectionHeader = NULL;
 	PIMAGE_DATA_DIRECTORY DataDirectory = NULL;
 
-	//Header信息
+	
 	pDosHeader = (PIMAGE_DOS_HEADER)pFileBuffer;
 	pNTHeader = (PIMAGE_NT_HEADERS)((DWORD)pFileBuffer + pDosHeader->e_lfanew);
 	pPEHeader = (PIMAGE_FILE_HEADER)(((DWORD)pNTHeader) + 4);
 	pOptionHeader = (PIMAGE_OPTIONAL_HEADER32)((DWORD)pPEHeader + IMAGE_SIZEOF_FILE_HEADER);
 	pSectionHeader = (PIMAGE_SECTION_HEADER)((DWORD)pOptionHeader + pPEHeader->SizeOfOptionalHeader);
 
-	//定位Directory_Data;
+	
 	DataDirectory = pOptionHeader->DataDirectory;
 
 	return (DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress)
